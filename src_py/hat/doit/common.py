1b09a7f9f3e7138cc66aafe6dbb49fd4b3bfeb06
bozo.kopic <bozo@kopic.xyz>
2021-08-06 00:26:57 +0200

    common wheel build

--- src_py/hat/doit/common.py
+++ src_py/hat/doit/common.py
@@ -5,6 +5,7 @@ import functools
 import itertools
 import multiprocessing
 import os
+import platform
 import shutil
 import subprocess
 import sys
@@ -66,6 +67,18 @@ def cp_r(src: os.PathLike, dest: os.PathLike):
         shutil.copy2(str(src), str(dest))
 
 
+def path_rglob(path: Path,
+               patterns: typing.List[str] = ['*'],
+               blacklist: typing.Set[str] = set()):
+    if path.name in blacklist:
+        return
+    if not path.is_dir():
+        yield path
+    for i in set(itertools.chain.from_iterable(path.glob(pattern)
+                                               for pattern in patterns)):
+        yield from path_rglob(i, patterns, blacklist)
+
+
 class StaticWebServer:
 
     def __init__(self, dir: os.PathLike, port: int):
@@ -119,6 +132,63 @@ def get_version(version_type: VersionType = VersionType.SEMVER,
         return packaging.version.Version(version).public
 
     raise ValueError()
+
+
+class License(enum.Enum):
+    APACHE2 = 'Apache-2.0'
+    GPL3 = 'GPLv3'
+
+
+def wheel_build(src_dir: Path,
+                dst_dir: Path,
+                src_paths: typing.List[Path],
+                name: str,
+                description: str,
+                url: str,
+                license: License,
+                packages: typing.List[str],
+                version_path: Path = Path('VERSION'),
+                readme_path: Path = Path('README.rst'),
+                license_path: typing.Optional[Path] = Path('LICENSE'),
+                requirements_path: typing.Optional[Path] = Path('requirements.pip.runtime.txt'),  # NOQA
+                python_tag: str = 'cp38.cp39',
+                platform_specific: bool = False,
+                console_scripts=[],
+                gui_scripts=[]):
+    rm_rf(dst_dir)
+    mkdir_p(dst_dir)
+
+    with open(dst_dir / 'MANIFEST.in', 'w', encoding='utf-8') as f:
+        for src_path in src_paths:
+            dst_path = dst_dir / src_path.relative_to(src_dir)
+            mkdir_p(dst_path.parent)
+            cp_r(src_path, dst_path)
+            f.write(f"include {dst_path.relative_to(dst_dir)}\n")
+
+    (dst_dir / 'setup.py').write_text(_wheel_setup_py.format(
+        name=repr(name),
+        version=repr(get_version(version_type=VersionType.PIP,
+                                 version_path=version_path)),
+        description=repr(description),
+        readme=repr(readme_path.read_text().strip()),
+        url=repr(url),
+        license=repr(license.value),
+        license_classifier=repr(_get_wheel_license_classifier(license)),
+        packages=repr(packages),
+        requirements=repr([str(i)
+                           for i in read_pip_requirements(requirements_path)]
+                          if requirements_path else []),
+        python_tag=repr(python_tag),
+        plat_name=repr(_get_wheel_plat_name(platform_specific)),
+        console_scripts=repr(console_scripts),
+        gui_scripts=repr(gui_scripts)))
+
+    if license_path:
+        cp_r(license_path, dst_dir / 'LICENSE')
+
+    subprocess.run([sys.executable, 'setup.py', '-q', 'bdist_wheel'],
+                   cwd=str(dst_dir),
+                   check=True)
 
 
 class SphinxOutputType(enum.Enum):
@@ -230,3 +300,78 @@ class CBuild:
         return list(itertools.chain.from_iterable(
             (Path(path) for path in i.replace(' \\\n', '').strip().split(' '))
             for i in content))
+
+
+def _get_wheel_license_classifier(license):
+    if license == License.APACHE2:
+        return 'License :: OSI Approved :: Apache Software License'
+
+    if license == License.GPL3:
+        return ('License :: OSI Approved :: '
+                'GNU General Public License v3 (GPLv3)')
+
+    raise ValueError('unsupported license')
+
+
+def _get_wheel_plat_name(platform_specific):
+    if not platform_specific:
+        return 'any'
+
+    arch, _ = platform.architecture()
+    if sys.platform == 'win32' and arch == '32bit':
+        return 'win32'
+    if sys.platform == 'win32' and arch == '64bit':
+        return 'win_amd64'
+    if sys.platform == 'linux' and arch == '64bit':
+        return 'manylinux1_x86_64'
+    if sys.platform == 'darwin' and arch == '64bit':
+        return 'macosx_10_13_x86_64'
+
+    raise NotImplementedError()
+
+
+_wheel_setup_py = r"""
+from setuptools import setup
+
+name = {name}
+version = {version}
+description = {description}
+readme = {readme}
+url = {url}
+license = {license}
+license_classifier = {license_classifier}
+packages = {packages}
+requirements = {requirements}
+python_tag = {python_tag}
+plat_name = {plat_name}
+console_scripts = {console_scripts}
+gui_scripts = {gui_scripts}
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    url=url,
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        license_classifier],
+    packages=packages,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
+    python_requires='>=3.8',
+    options={{
+        'bdist_wheel': {{
+            'python_tag': python_tag,
+            'py_limited_api': python_tag,
+            'plat_name': 'any'
+        }}
+    }},
+    entry_points={{
+        'console_scripts': console_scripts,
+        'gui_scripts': gui_scripts
+    }})
+"""
