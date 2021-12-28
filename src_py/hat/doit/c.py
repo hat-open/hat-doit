c4b3c0f9748c986c76b0db822541885f03835d95
bozo.kopic <bozo@kopic.xyz>
2021-12-28 20:16:45 +0100

    c build - cross-compile api change

--- src_py/hat/doit/c.py
+++ src_py/hat/doit/c.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import enum
 import itertools
 import os
 import sys
@@ -7,19 +8,65 @@ import typing
 from . import common
 
 
-if sys.platform == 'win32':
-    exe_suffix = '.exe'
-    lib_suffix = '.dll'
-elif sys.platform == 'darwin':
-    exe_suffix = ''
-    lib_suffix = '.dylib'
-else:
-    exe_suffix = ''
-    lib_suffix = '.so'
+class Platform(enum.Enum):
+    WINDOWS = 'win32'
+    DARWIN = 'darwin'
+    LINUX = 'linux'
 
-cpp = os.environ.get('CPP', 'cpp')
-cc = os.environ.get('CC', 'cc')
-ld = os.environ.get('LD', 'cc')
+
+def get_exe_suffix(platform: Platform) -> str:
+    if platform == Platform.WINDOWS:
+        return '.exe'
+
+    if platform == Platform.DARWIN:
+        return ''
+
+    if platform == Platform.LINUX:
+        return ''
+
+    raise ValueError('unsupported platform')
+
+
+def get_lib_suffix(platform: Platform) -> str:
+    if platform == Platform.WINDOWS:
+        return '.dll'
+
+    if platform == Platform.DARWIN:
+        return '.dylib'
+
+    if platform == Platform.LINUX:
+        return '.so'
+
+    raise ValueError('unsupported platform')
+
+
+local_platform: Platform = Platform(sys.platform)
+local_exe_suffix: str = get_exe_suffix(local_platform)
+local_lib_suffix: str = get_lib_suffix(local_platform)
+
+
+def get_cpp(platform: Platform) -> str:
+    if platform == local_platform:
+        return os.environ.get('CPP', 'cpp')
+
+    if (local_platform, platform) == (Platform.LINUX, Platform.WINDOWS):
+        return 'x86_64-w64-mingw32-cpp'
+
+    raise ValueError('unsupported platform')
+
+
+def get_cc(platform: Platform) -> str:
+    if platform == local_platform:
+        return os.environ.get('CC', 'cc')
+
+    if (local_platform, platform) == (Platform.LINUX, Platform.WINDOWS):
+        return 'x86_64-w64-mingw32-cc'
+
+    raise ValueError('unsupported platform')
+
+
+def get_ld(platform: Platform) -> str:
+    return get_cc(platform)
 
 
 class CBuild:
@@ -28,9 +75,7 @@ class CBuild:
                  src_paths: typing.List[Path],
                  build_dir: Path, *,
                  src_dir: Path = Path('.'),
-                 cpp: str = cpp,
-                 cc: str = cc,
-                 ld: str = ld,
+                 platform: Platform = local_platform,
                  cpp_flags: typing.List[str] = [],
                  cc_flags: typing.List[str] = [],
                  ld_flags: typing.List[str] = [],
@@ -38,9 +83,7 @@ class CBuild:
         self._src_paths = src_paths
         self._build_dir = build_dir
         self._src_dir = src_dir
-        self._cpp = cpp
-        self._cc = cc
-        self._ld = ld
+        self._platform = platform
         self._cpp_flags = cpp_flags
         self._cc_flags = cc_flags
         self._ld_flags = ld_flags
@@ -49,27 +92,30 @@ class CBuild:
     def get_task_exe(self, exe_path: Path) -> typing.Dict:
         obj_paths = [self._get_obj_path(src_path)
                      for src_path in self._src_paths]
-        return {'actions': [(common.mkdir_p, [exe_path.parent]),
-                            [self._ld,
-                             *(str(obj_path) for obj_path in obj_paths),
-                             '-o', str(exe_path),
-                             *self._ld_flags]],
-                'file_dep': obj_paths,
-                'task_dep': self._task_dep,
-                'targets': [exe_path]}
+        yield {'name': str(exe_path),
+               'actions': [(common.mkdir_p, [exe_path.parent]),
+                           [get_ld(self._platform),
+                            *(str(obj_path) for obj_path in obj_paths),
+                            '-o', str(exe_path),
+                            *self._ld_flags]],
+               'file_dep': obj_paths,
+               'task_dep': self._task_dep,
+               'targets': [exe_path]}
 
     def get_task_lib(self, lib_path: Path) -> typing.Dict:
         obj_paths = [self._get_obj_path(src_path)
                      for src_path in self._src_paths]
-        shared_flag = '-mdll' if sys.platform == 'win32' else '-shared'
-        return {'actions': [(common.mkdir_p, [lib_path.parent]),
-                            [self._ld,
-                             *(str(obj_path) for obj_path in obj_paths),
-                             '-o', str(lib_path), shared_flag,
-                             *self._ld_flags]],
-                'file_dep': obj_paths,
-                'task_dep': self._task_dep,
-                'targets': [lib_path]}
+        shared_flag = ('-mdll' if self._platform == Platform.WINDOWS
+                       else '-shared')
+        yield {'name': str(lib_path),
+               'actions': [(common.mkdir_p, [lib_path.parent]),
+                           [get_ld(self._platform),
+                            *(str(obj_path) for obj_path in obj_paths),
+                            '-o', str(lib_path), shared_flag,
+                            *self._ld_flags]],
+               'file_dep': obj_paths,
+               'task_dep': self._task_dep,
+               'targets': [lib_path]}
 
     def get_task_objs(self) -> typing.Dict:
         for src_path in self._src_paths:
@@ -78,7 +124,8 @@ class CBuild:
             header_paths = self._parse_dep(dep_path)
             yield {'name': str(obj_path),
                    'actions': [(common.mkdir_p, [obj_path.parent]),
-                               [cc, *self._cpp_flags, *self._cc_flags, '-c',
+                               [get_cc(self._platform),
+                                *self._cpp_flags, *self._cc_flags, '-c',
                                 '-o', str(obj_path), str(src_path)]],
                    'file_dep': [src_path, dep_path, *header_paths],
                    'task_dep': self._task_dep,
@@ -89,7 +136,8 @@ class CBuild:
             dep_path = self._get_dep_path(src_path)
             yield {'name': str(dep_path),
                    'actions': [(common.mkdir_p, [dep_path.parent]),
-                               [cpp, *self._cpp_flags, '-MM',
+                               [get_cpp(self._platform),
+                                *self._cpp_flags, '-MM',
                                 '-o', str(dep_path), str(src_path)]],
                    'file_dep': [src_path],
                    'task_dep': self._task_dep,
