1170b850c5d443330ea908d678a3945eb0e0072c
bozo.kopic <bozo@kopic.xyz>
2023-01-14 00:17:59 +0100

    c build refactoring

--- src_py/hat/doit/c.py
+++ src_py/hat/doit/c.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+import functools
 import importlib.resources
 import os
+import shlex
+import shutil
 import subprocess
 import sysconfig
 import typing
@@ -8,7 +11,8 @@ import typing
 from . import common
 
 
-def get_exe_suffix(platform: common.Platform) -> str:
+def get_exe_suffix(platform: common.Platform = common.target_platform
+                   ) -> str:
     if platform == common.Platform.WINDOWS_AMD64:
         return '.exe'
 
@@ -23,7 +27,8 @@ def get_exe_suffix(platform: common.Platform) -> str:
     raise ValueError('unsupported platform')
 
 
-def get_lib_suffix(platform: common.Platform) -> str:
+def get_lib_suffix(platform: common.Platform = common.target_platform
+                   ) -> str:
     if platform == common.Platform.WINDOWS_AMD64:
         return '.dll'
 
@@ -40,10 +45,10 @@ def get_lib_suffix(platform: common.Platform) -> str:
     raise ValueError('unsupported platform')
 
 
-def get_ext_suffix(platform: common.Platform,
-                   py_version: common.PyVersion,
-                   py_limited_api: typing.Optional[common.PyVersion]
-                   ) -> str:
+def get_py_ext_suffix(platform: common.Platform = common.target_platform,
+                      py_version: common.PyVersion = common.target_py_version,
+                      py_limited_api: typing.Optional[common.PyVersion] = None
+                      ) -> str:
     _, major, minor = (py_limited_api or py_version).value
 
     if platform == common.Platform.WINDOWS_AMD64:
@@ -96,59 +101,56 @@ local_lib_suffix: str = get_lib_suffix(common.local_platform)
 target_lib_suffix: str = get_lib_suffix(common.target_platform)
 
 
-def get_local_ext_suffix(py_limited_api: typing.Optional[common.PyVersion]
-                         ) -> str:
-    return get_ext_suffix(common.local_platform, common.local_py_version,
-                          py_limited_api)
+@functools.lru_cache
+def get_cc(platform: common.Platform = common.target_platform
+           ) -> str:
+    candidates = []
 
-
-def get_target_ext_suffix(py_limited_api: typing.Optional[common.PyVersion]
-                          ) -> str:
-    return get_ext_suffix(common.target_platform, common.target_py_version,
-                          py_limited_api)
-
-
-def get_cpp(platform: common.Platform) -> str:
     if platform == common.local_platform:
-        return os.environ.get('CPP', 'cpp')
+        if 'CC' in os.environ:
+            candidates.append(os.environ['CC'])
+        candidates.append('cc')
+        candidates.append('gcc')
 
-    if common.local_platform == common.Platform.LINUX_GNU_X86_64:
-        if platform == common.Platform.WINDOWS_AMD64:
-            return 'x86_64-w64-mingw32-cpp'
+    if platform == common.Platform.WINDOWS_AMD64:
+        candidates.append('x86_64-w64-mingw32-gcc')
 
-        if platform == common.Platform.LINUX_GNU_AARCH64:
-            return 'aarch64-linux-gnu-cpp'
+    elif platform == common.Platform.LINUX_GNU_AARCH64:
+        candidates.append('aarch64-linux-gnu-gcc')
 
-        if platform == common.Platform.LINUX_MUSL_X86_64:
-            return 'musl-gcc'
+    elif platform == common.Platform.LINUX_GNU_ARMV7L:
+        candidates.append('arm-linux-gnueabihf-gcc')
+
+    elif platform == common.Platform.LINUX_MUSL_X86_64:
+        candidates.append('musl-gcc')
+
+    for candidate in candidates:
+        cmd = shutil.which(candidate)
+        if cmd:
+            return cmd
 
     raise ValueError('unsupported platform')
 
 
-def get_cc(platform: common.Platform) -> str:
-    if platform == common.local_platform:
-        return os.environ.get('CC', 'cc')
+def get_c_flags(platform: common.Platform = common.target_platform
+                ) -> typing.Iterable[str]:
+    yield from shlex.split(os.environ.get('CFLAGS', ''))
 
-    if common.local_platform == common.Platform.LINUX_GNU_X86_64:
-        if platform == common.Platform.WINDOWS_AMD64:
-            return 'x86_64-w64-mingw32-cc'
-
-        if platform == common.Platform.LINUX_GNU_AARCH64:
-            return 'aarch64-linux-gnu-gcc'
-
-        if platform == common.Platform.LINUX_MUSL_X86_64:
-            return 'musl-gcc'
-
-    raise ValueError('unsupported platform')
+    if platform != common.local_platform:
+        if platform == common.Platform.LINUX_GNU_ARMV7L:
+            yield '-march=armv7'
 
 
-def get_ld(platform: common.Platform) -> str:
-    return get_cc(platform)
+def get_ld_flags(platform: common.Platform = common.target_platform
+                 ) -> typing.Iterable[str]:
+    yield from shlex.split(os.environ.get('LDFLAGS', ''))
 
 
-def get_py_cpp_flags(py_limited_api: typing.Optional[common.PyVersion]
-                     ) -> typing.Iterable[str]:
-    _, major, minor = common.target_py_version.value
+def get_py_c_flags(platform: common.Platform = common.target_platform,
+                   py_version: common.PyVersion = common.target_py_version,
+                   py_limited_api: typing.Optional[common.PyVersion] = None
+                   ) -> typing.Iterable[str]:
+    _, major, minor = py_version.value
 
     yield '-DPY_SSIZE_T_CLEAN'
 
@@ -156,8 +158,8 @@ def get_py_cpp_flags(py_limited_api: typing.Optional[common.PyVersion]
         _, limited_major, limited_minor = py_limited_api.value
         yield f'-DPy_LIMITED_API=0x{limited_major:02X}{limited_minor:02X}0000'
 
-    if common.target_platform == common.local_platform:
-        if common.target_py_version == common.local_py_version:
+    if platform == common.local_platform:
+        if py_version == common.local_py_version:
             include_path = sysconfig.get_path('include')
             if include_path:
                 yield f'-I{include_path}'
@@ -169,13 +171,13 @@ def get_py_cpp_flags(py_limited_api: typing.Optional[common.PyVersion]
             raise ValueError('unsupported version')
 
     elif common.local_platform == common.Platform.LINUX_GNU_X86_64:
-        if common.target_platform == common.Platform.WINDOWS_AMD64:
+        if platform == common.Platform.WINDOWS_AMD64:
             yield f'-I/usr/x86_64-w64-mingw32/include/python{major}{minor}'
 
-        elif common.target_platform in (common.Platform.LINUX_GNU_AARCH64,
-                                        common.Platform.LINUX_GNU_ARMV7L,
-                                        common.Platform.LINUX_MUSL_X86_64,
-                                        common.Platform.LINUX_MUSL_AARCH64):
+        elif platform in (common.Platform.LINUX_GNU_AARCH64,
+                          common.Platform.LINUX_GNU_ARMV7L,
+                          common.Platform.LINUX_MUSL_X86_64,
+                          common.Platform.LINUX_MUSL_AARCH64):
             yield f'-I/usr/include/python{major}.{minor}'
 
         else:
@@ -185,11 +187,13 @@ def get_py_cpp_flags(py_limited_api: typing.Optional[common.PyVersion]
         raise ValueError('unsupported platform')
 
 
-def get_py_ld_flags(py_limited_api: typing.Optional[common.PyVersion]
+def get_py_ld_flags(platform: common.Platform = common.target_platform,
+                    py_version: common.PyVersion = common.target_py_version,
+                    py_limited_api: typing.Optional[common.PyVersion] = None
                     ) -> typing.Iterable[str]:
-    _, major, minor = common.target_py_version.value
+    _, major, minor = py_version.value
 
-    if common.target_platform == common.local_platform:
+    if platform == common.local_platform:
         if common.local_platform == common.Platform.DARWIN_X86_64:
             stdlib_path = (Path(sysconfig.get_path('stdlib')) /
                            f'config-{major}.{minor}-darwin')
@@ -199,13 +203,20 @@ def get_py_ld_flags(py_limited_api: typing.Optional[common.PyVersion]
             data_path = sysconfig.get_path('data')
             yield f"-L{data_path}"
 
-    if common.target_platform == common.Platform.WINDOWS_AMD64:
+
+def get_py_ld_libs(platform: common.Platform = common.target_platform,
+                   py_version: common.PyVersion = common.target_py_version,
+                   py_limited_api: typing.Optional[common.PyVersion] = None
+                   ) -> typing.Iterable[str]:
+    _, major, minor = py_version.value
+
+    if platform == common.Platform.WINDOWS_AMD64:
         if py_limited_api:
             yield f"-lpython{major}"
         else:
             yield f"-lpython{major}{minor}"
 
-    elif common.target_platform == common.Platform.DARWIN_X86_64:
+    elif platform == common.Platform.DARWIN_X86_64:
         if py_limited_api:
             yield f"-lpython{major}"
         else:
@@ -236,17 +247,17 @@ class CBuild:
                  build_dir: Path, *,
                  src_dir: Path = Path('.'),
                  platform: common.Platform = common.target_platform,
-                 cpp_flags: typing.List[str] = [],
-                 cc_flags: typing.List[str] = [],
+                 c_flags: typing.List[str] = [],
                  ld_flags: typing.List[str] = [],
+                 ld_libs: typing.List[str] = [],
                  task_dep: typing.List[str] = []):
         self._src_paths = src_paths
         self._build_dir = build_dir
         self._src_dir = src_dir
         self._platform = platform
-        self._cpp_flags = cpp_flags
-        self._cc_flags = cc_flags
+        self._c_flags = c_flags
         self._ld_flags = ld_flags
+        self._ld_libs = ld_libs
         self._task_dep = task_dep
 
     def get_task_exe(self, exe_path: Path) -> typing.Dict:
@@ -254,10 +265,12 @@ class CBuild:
                      for src_path in self._src_paths]
         yield {'name': str(exe_path),
                'actions': [(common.mkdir_p, [exe_path.parent]),
-                           [get_ld(self._platform),
-                            *(str(obj_path) for obj_path in obj_paths),
+                           [get_cc(self._platform),
+                            *get_ld_flags(self._platform),
+                            *self._ld_flags,
                             '-o', str(exe_path),
-                            *self._ld_flags]],
+                            *(str(obj_path) for obj_path in obj_paths),
+                            *self._ld_libs]],
                'file_dep': obj_paths,
                'task_dep': self._task_dep,
                'targets': [exe_path]}
@@ -270,10 +283,13 @@ class CBuild:
             else '-shared')
         yield {'name': str(lib_path),
                'actions': [(common.mkdir_p, [lib_path.parent]),
-                           [get_ld(self._platform),
+                           [get_cc(self._platform),
+                            shared_flag,
+                            *get_ld_flags(self._platform),
+                            *self._ld_flags,
+                            '-o', str(lib_path),
                             *(str(obj_path) for obj_path in obj_paths),
-                            '-o', str(lib_path), shared_flag,
-                            *self._ld_flags]],
+                            *self._ld_libs]],
                'file_dep': obj_paths,
                'task_dep': self._task_dep,
                'targets': [lib_path]}
@@ -286,8 +302,11 @@ class CBuild:
             yield {'name': str(obj_path),
                    'actions': [(common.mkdir_p, [obj_path.parent]),
                                [get_cc(self._platform),
-                                *self._cpp_flags, *self._cc_flags, '-c',
-                                '-o', str(obj_path), str(src_path)]],
+                                '-c',
+                                *get_c_flags(self._platform),
+                                *self._c_flags,
+                                '-o', str(obj_path),
+                                str(src_path)]],
                    'file_dep': [src_path, dep_path, *header_paths],
                    'task_dep': self._task_dep,
                    'targets': [obj_path]}
@@ -297,9 +316,12 @@ class CBuild:
             dep_path = self._get_dep_path(src_path)
             yield {'name': str(dep_path),
                    'actions': [(common.mkdir_p, [dep_path.parent]),
-                               [get_cpp(self._platform),
-                                *self._cpp_flags, '-MM',
-                                '-o', str(dep_path), str(src_path)]],
+                               [get_cc(self._platform),
+                                '-MM',
+                                *get_c_flags(self._platform),
+                                *self._c_flags,
+                                '-o', str(dep_path),
+                                str(src_path)]],
                    'file_dep': [src_path],
                    'task_dep': self._task_dep,
                    'targets': [dep_path]}
