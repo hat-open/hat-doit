75c0acb5773fac439acee46eff791cb832da5023
bozo.kopic <bozo@kopic.xyz>
2024-03-08 17:24:11 +0100

    js run eslint update

--- src_py/hat/doit/js.py
+++ src_py/hat/doit/js.py
@@ -137,10 +137,20 @@ class ESLintConf(enum.Enum):
 
 def run_eslint(path: Path,
                conf: ESLintConf = ESLintConf.JS):
+    if conf == ESLintConf.JS:
+        parser = 'espree'
+
+    elif conf == ESLintConf.TS:
+        parser = '@typescript-eslint/parser'
+
+    else:
+        raise ValueError('unsupported conf')
+
     package = importlib.resources.files(hat_doit_eslint)
     with importlib.resources.as_file(package /
                                      f'{conf.value}.yaml') as conf_path:
         subprocess.run(['npx', 'eslint',
+                        '--parser', parser,
                         '--resolve-plugins-relative-to', '.',
                         '-c', str(conf_path),
                         str(path)],
